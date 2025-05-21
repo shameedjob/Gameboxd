6ad5fd51404b6ed5e3612fa9e7867e76000dfdca
Shameed Job <shameed.job.32@gmail.com>
2025-05-21 01:32:10 -0400

    add firebase

--- services/firebase.py
+++ services/firebase.py
@@ -3,9 +3,23 @@
 
 import firebase_admin # type: ignore
 from firebase_admin import credentials, firestore  # type: ignore
+import os
 
+mydict = {
+    "type": "service_account",
+    "project_id": "video-game-letterboxd-project",
+    "private_key_id": os.environ.get("PRIVATE_KEY_ID"),
+    "private_key": os.environ.get("PRIVATE_KEY"),
+    "client_email": "firebase-adminsdk-fbsvc@video-game-letterboxd-project.iam.gserviceaccount.com",
+    "client_id": "110980016356798526002",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40video-game-letterboxd-project.iam.gserviceaccount.com",
+    "universe_domain": "googleapis.com"
+  }
 # Initialize Firebase
-cred = credentials.Certificate("firebase-key.json")
+cred = credentials.Certificate(mydict)
 firebase_app = firebase_admin.initialize_app(cred)
 db = firestore.client()
 
